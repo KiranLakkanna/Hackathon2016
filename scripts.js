f109adf1a9d6a51c35d94a817e1bc6a2495df8cf
Nayak <WX88@global.Tesco.org>
2016-09-22 15:24:46 +0530

    integerated code base

--- scripts.js
+++ scripts.js
@@ -64,7 +64,9 @@ $(document).ready(function() {
     document.getElementById("offerMsg").innerText = offerCategory
     var imageSource = SpecialOffers[key].imag
     $('#offerMsg').append('<div style="display:inline;max-width:80%;float:right" class="container"> <br> <div id="myCarouse2" style="max-width: 70%;"class="carousel slide" data-ride="carousel"><ol class="carousel-indicators"><li data-target="#myCarouse2" data-slide-to="0" class="active"></li> <li data-target="#myCarousel" data-slide-to="1"></li> <li data-target="#myCarouse2" data-slide-to="2"></li> <li data-target="#myCarouse2" data-slide-to="3"></li> </ol> <div class="carousel-inner" role="listbox"> <div class="item active"> <img src="./images/vegetables.jpg" alt="Chania" width="460" height="300"> </div> <div class="item "> <img src="./images/choco.jpg" alt="Chania" width="460" height="300"> </div> <div class="item"><img src="./images/chips.jpg" alt="Flower" width="460" height="300"> </div> <div class="item"> <img src="./images/food.jpg" alt="Flower" width="460" height="300"> </div> </div> <a class="left carousel-control" href="#myCarouse2" role="button" data-slide="prev"><span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span> <span class="sr-only">Previous</span> </a> <a class="right carousel-control" href="#myCarouse2" role="button" data-slide="next"> <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span> <span class="sr-only">Next</span> </a> </div> </div>');
-
+if(document.getElementById('myImage')!==null){
     document.getElementById('myImage').src = imageSource;
+}
+
 });
 
